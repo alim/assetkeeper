df6a84da587bcbf4bc067f593bb0185d9d8422ec
Andy Lim <alim@nspartners.com>
2015-02-24 17:35:12 -0500

    Another update to Organization setup for specs to reduce CI errors.

--- spec/shared/organization_setup.rb
+++ spec/shared/organization_setup.rb
@@ -3,8 +3,8 @@ shared_context 'organization_setup' do
 
   # Single organization with multiple users
 	let(:single_organization_with_users) {
-		5.times.each { FactoryGirl.create(:user_with_account) }
-		@owner = User.last
+		4.times.each { FactoryGirl.create(:user_with_account) }
+		@owner = FactoryGirl.create(:user_with_account)
 		@organization = FactoryGirl.create(:organization, owner: @owner)
 		User.all.each {|user| @organization.users << user}
 	}
