35b685c1166cb759b3bfae7d7c7076ca24acd8fc
Joe Milligan <jmilligan@nspartners.com>
2015-07-12 15:12:33 -0400

    Initial Category Model

---
+++ app/models/category.rb
@@ -0,0 +1,43 @@
+#######################################################################
+# The Category class is the primary model for holding information about
+# infrastructure categories.
+#######################################################################
+
+class Category
+  include Mongoid::Document
+
+   # Add call to strip leading and trailing white spaces from all attributes
+  strip_attributes  # See strip_attributes for more information
+
+  ## CONSTANTS --------------------------------------------------------
+
+
+  ## FIELDS -----------------------------------------------------------
+
+  field :name, type: String
+  field :description, type: String
+  field :is_leaf, type: Mongoid::Boolean
+
+  ## RELATIONSHIPS ----------------------------------------------------
+
+  belongs_to :parent,   :class_name => :category
+  has_many   :children, :class_name => :category
+
+  ## DELEGATIONS ------------------------------------------------------
+
+
+  ## VALIDATIONS ------------------------------------------------------
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  validates_presence_of :description
+  validates_presence_of :is_leaf
+
+  ## INDICES & SCOPES -------------------------------------------------
+
+  ## PUBLIC INSTANCE METHODS ------------------------------------------
+
+  ## CLASS METHODS ----------------------------------------------------
+
+
+end
